fde4d0d7605cd98a2367083e750549d9a61faf93
miajio <miajio@163.com>
2025-06-04 17:57:14 +0800

    [update] 加入注释

--- option.go
+++ option.go
@@ -15,36 +15,42 @@ type Config struct {
 	Timeout            time.Duration // time out
 }
 
+// WithApiKey 设置api key
 func WithApiKey(apiKey string) Option {
 	return func(c *Config) {
 		c.ApiKey = apiKey
 	}
 }
 
+// WithBaseUrl 设置base url
 func WithBaseUrl(baseUrl string) Option {
 	return func(c *Config) {
 		c.BaseUrl = baseUrl
 	}
 }
 
+// WithTimeout 设置超时时间
 func WithTimeout(timeout time.Duration) Option {
 	return func(c *Config) {
 		c.Timeout = timeout
 	}
 }
 
+// WithModelsUrl 配置模型列表的url
 func WithModelsUrl(modelsUrl string) Option {
 	return func(c *Config) {
 		c.ModelsUrl = modelsUrl
 	}
 }
 
+// WithBalanceUrl 配置余额的url
 func WithBalanceUrl(balanceUrl string) Option {
 	return func(c *Config) {
 		c.BalanceUrl = balanceUrl
 	}
 }
 
+// WithChatCompletionsUrl 配置对话的url
 func WithChatCompletionsUrl(chatCompletionsUrl string) Option {
 	return func(c *Config) {
 		c.ChatCompletionsUrl = chatCompletionsUrl
